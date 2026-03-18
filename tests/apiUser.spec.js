3b2f09a039b80eabc25e45508a29b39b66142f00
Marriamer <mgalindocastillo6@gmail.com>
2026-03-18 15:27:25 -0500

    Improve API test with JSON validation

--- tests/apiUser.spec.js
+++ tests/apiUser.spec.js
@@ -4,8 +4,12 @@ test('GET users API test', async ({ request }) => {
 
   const response = await request.get('https://jsonplaceholder.typicode.com/users');
 
-  console.log(await response.json());
-
   expect(response.status()).toBe(200);
+
+  const users = await response.json();
+
+  expect(users.length).toBeGreaterThan(0);
+
+  expect(users[0]).toHaveProperty('name');
 
 });
